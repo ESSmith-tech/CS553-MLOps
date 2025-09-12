2851136efda73c639a074a4e4ebbea5d624abad5
Quinn Williams <145822154+WinnQuilliams@users.noreply.github.com>
2025-09-12 17:52:41 -0400

    Update ui_factory.py

--- src/ui_factory.py
+++ src/ui_factory.py
@@ -24,7 +24,7 @@ class UIFactory:
                 return str(evt.value['caption'])
 
             gr.Markdown(
-                "<span style='color: white; font-size: 30px'>Choose your Philosopher</span>"
+                value=config["ui"]["value"]
             )
 
             # Gallery at the top
